d63dca318ab9ae232697ba748e4785f23d334271
Suhana <ssuhana_be22@thapar.edu>
2024-08-08 13:56:20 +0530

    APP for audio to text conversion

--- Audio2Text.py
+++ Audio2Text.py
@@ -7,18 +7,12 @@ transcriber = aai.Transcriber()
 
 transcript =transcriber.transcribe(FILE_URL)
 
-# if transcript.status == aai.TranscriptStatus.error:
-#     print(transcript.error)
-# else:
-#     print(transcript.text)
-
 if transcript.status == aai.TranscriptStatus.error:
     print(f"Error: {transcript.error}")
 else:
-    # Print the transcription text
     transcription_text = transcript.text
     print(f"Transcription: {transcription_text}")
 
-    # Save the transcription to a text file
+
     with open("text.txt", "w") as text_file:
         text_file.write(transcription_text)
